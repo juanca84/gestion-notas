b0fe72f330495cf10944f670478c6820e95ba712
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-05 20:48:50 -0400

    Creación de proyecto VueJS

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hola Mundo'
+    }
+  }
+}
+</script>
+
+<style>
+  .form {
+    text-align: left;
+  }
+</style>
